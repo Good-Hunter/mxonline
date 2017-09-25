835bb10c8613574b535a179e3635305289265726
Good-Hunter <568091431@qq.com>
2017-09-25 09:38:01 +0800

    Update settings.py

--- mxonline/mxonline/settings.py
+++ mxonline/mxonline/settings.py
@@ -89,16 +89,13 @@ WSGI_APPLICATION = 'mxonline.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': "mxonline",
-        'USER':"root",
-        'PASSWORD':'root',
-        'HOST':'127.0.0.1'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
